7a05c3f5817003f700d73c910fb2f51f488a3a35
allamurodxakimov <xakimov2806@gmail.com>
2023-11-17 08:37:05 +0300

    class create

--- create_one_object.py
+++ create_one_object.py
@@ -1,11 +1,13 @@
 from create_one_attribute import Person
 
 #create an object named "person" whose name is "Ali"
-class Ali:
-    def __init__(self,name,fam,year,age):
-        self.name = name
-        self.famliy = fam
-        self.year = year
-        self.age = age
-func = Ali("Alijon","Valiyev","1995-yil",28)
-print(func.name,func.famliy,func.year,func.age)
+# class Ali:
+#     def __init__(self,name,fam,year,age):
+#         self.name = name
+#         self.famliy = fam
+#         self.year = year
+#         self.age = age
+# func = Ali("Alijon","Valiyev","1995-yil",28)
+# print(func.name,func.famliy,func.year,func.age)
+x = Person("Alijon")
+print(x.name)
