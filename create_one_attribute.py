01eaa4693c1f666dbf8806278c5ace0bcae603a6
allamurodxakimov <xakimov2806@gmail.com>
2023-11-17 08:37:47 +0300

    class one atribute

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,9 +1,14 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
-class Person:
-    def __init__(self,name,lname,age:int):
-        self.last_name = lname
-        self.name = name
-        self.age = age
-x = Person("Allamurod","Xakimov",18)
+# class Person:
+#     def __init__(self,name,lname,age:int):
+#         self.last_name = lname
+#         self.name = name
+#         self.age = age
+# x = Person("Allamurod","Xakimov",18)
 # print(x.name,x.last_name,x.age,"yoshda")
+class Person:
+    def __init__(self,name):
+        self.name = name
+x = Person("Allamurod")
+# print(x.name)
