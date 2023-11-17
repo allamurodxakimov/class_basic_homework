6637fd6d62fc3164daedd03104ecb5ce0f615bad
allamurodxakimov <xakimov2806@gmail.com>
2023-11-17 08:32:00 +0300

    class person

--- person.py
+++ person.py
@@ -1,6 +1,6 @@
 #Create a "Person" class
 class Person:
-    def __init__(self,name):
-        self.name = name
-data = Person("Allamurod")
-# print(data.name)
+    def __init__(self):
+        pass
+data = Person()
+print(data)
